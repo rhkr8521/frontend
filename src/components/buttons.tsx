0c89abcef077b206384e99f24bd1c72f23e7b344
Minjeong Kim <80758613+Minnnning@users.noreply.github.com>
2023-08-22 02:42:18 +0900

    Feature/memo btn feature (#4)
    * chore: 리드미 수정
    
    * chore: 리드미 수정
    
    * add menu button
    
    * menu css modify
    
    * memo secoll feature
    
    * marker click event
    
    * readme modify
    
    * location add button toggle
    
    * loation add marker img
    
    * react-modal 라이브러리 추가
    
    * 모달창 추가 태그 이미지 추가 필요
    
    * 모달창 추가

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,19 +1,50 @@
 import './css/menu.css';
+import ModalOpen from './modalOpen';
 import { MapMarker } from 'react-kakao-maps-sdk';
+import { useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Buttons(props: any) {
   const location = props.data;
+  const [toggle, setToggle] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const clickedToggle = () => {
+    setToggle((prev) => !prev);
+  };
+  const modalClose = () => {
+    setModalIsOpen(false);
+  };
+
+  // console.log('toggle', toggle);
+  console.log('modal', modalIsOpen);
+  // console.log('위치', location);
+
   return (
     <div>
-      <button
-        className="customButton"
-        style={{
-          bottom: '8em',
-          background:
-            'url(//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png) no-repeat -194px -450px',
-        }}
-      ></button>
+      {toggle ? (
+        // 추가 버튼 활성화
+        <button
+          className="customButton"
+          style={{
+            bottom: '8em',
+            background:
+              'url(//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png) no-repeat -194px -350px',
+          }}
+          onClick={clickedToggle}
+        ></button>
+      ) : (
+        // 추가 버튼 비활성화
+        <button
+          className="customButton"
+          style={{
+            bottom: '8em',
+            background:
+              'url(//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png) no-repeat -194px -450px',
+          }}
+          onClick={clickedToggle}
+        ></button>
+      )}
+
       <button
         className="customButton"
         style={{
@@ -22,6 +53,7 @@ function Buttons(props: any) {
             'url(//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png) no-repeat -105px -450px',
         }}
       ></button>
+
       <button
         className="customButton"
         style={{
@@ -30,7 +62,34 @@ function Buttons(props: any) {
             'url(//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png) no-repeat -153px -450px',
         }}
       ></button>
-      {location && <MapMarker position={location} />}
+      {toggle && (
+        <>
+          <MapMarker
+            position={location}
+            image={{
+              src: './public_assets/addMarker.png',
+              size: { width: 200, height: 60 },
+              options: {
+                offset: {
+                  x: 16,
+                  y: 52,
+                },
+              },
+            }}
+            onClick={() => {
+              setModalIsOpen(true);
+            }}
+          />
+        </>
+      )}
+      {modalIsOpen && (
+        <ModalOpen
+          data={location}
+          close={modalClose}
+          modalState={modalIsOpen}
+          markerSign={clickedToggle}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/modalOpen.tsx
@@ -0,0 +1,118 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Modal from 'react-modal';
+import { useState } from 'react';
+import './css/modal.css';
+
+function ModalOpen(props: any) {
+  const customModalStyles: ReactModal.Styles = {
+    overlay: {
+      backgroundColor: ' rgba(0, 0, 0, 0.4)',
+      width: '100%',
+      height: '100vh',
+      zIndex: '11',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+    },
+    content: {
+      width: '80%',
+      zIndex: '12',
+      minHeight: '47%',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '10px',
+      boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.25)',
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      overflow: 'auto',
+    },
+  };
+
+  const [values, setValues] = useState({
+    tag: '',
+    content: '',
+    lat: props.data.lat,
+    lng: props.data.lng,
+    image: '',
+    user: 'anyone', //유저이 이름을 가져와서 여기 대입
+  });
+
+  const handleChange = (e: any) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    alert(JSON.stringify(values, null, 2) + '\n메모가 추가되었습니다');
+    props.close(); //입력 창 닫기
+    props.markerSign(); //마커 추가 활성화 버튼 끄기
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={props.modalState}
+        onRequestClose={props.close}
+        style={customModalStyles}
+        ariaHideApp={false}
+        shouldCloseOnOverlayClick={false}
+      >
+        <div className="modal">
+          <h2>메모 작성</h2>
+          <form
+            onSubmit={handleSubmit}
+            method="post"
+            encType="multipart/form-data"
+          >
+            <p>
+              태그:
+              <select name="tag" value={values.tag} onChange={handleChange}>
+                <option selected value="기타">
+                  기타
+                </option>
+                <option value="쓰레기통">쓰레기통</option>
+                <option value="화장실">화장실</option>
+                <option value="공사중">공사중</option>
+              </select>
+            </p>
+            <p>
+              내용:
+              <input
+                type="text"
+                name="content"
+                value={values.content}
+                onChange={handleChange}
+              />
+            </p>
+            <p>
+              이미지:
+              <input
+                className="upload-name"
+                type="file"
+                name="image"
+                accept="image/*"
+                value={values.image}
+                onChange={handleChange}
+                placeholder="이미지를 업로드 하세요"
+              />
+            </p>
+            <div className="buttons">
+              <button type="submit" className="save-button">
+                저장
+              </button>
+              <button onClick={props.close} className="close-button">
+                닫기
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </>
+  );
+}
+export default ModalOpen;
